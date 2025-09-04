93df14210e1b522287544a3284c855aaeb8f53fb
valpoe <94831560+Valpoe@users.noreply.github.com>
2025-09-04 22:25:35 +0300

    wip

--- src/main/java/com/resizablechatbox/ResizableChatboxConfig.java
+++ src/main/java/com/resizablechatbox/ResizableChatboxConfig.java
@@ -3,11 +3,24 @@ package com.resizablechatbox;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Range;
 
 @ConfigGroup("resizablechatbox")
 public interface ResizableChatboxConfig extends Config {
 	@ConfigItem(keyName = "greeting", name = "Welcome Greeting", description = "The message to show to the user when they login")
 	default String greeting() {
 		return "Hello";
+	}
+
+	@Range(min = 28, max = 350)
+	@ConfigItem(keyName = "chatboxHeight", name = "Chatbox Height", description = "Set the height of the chatbox")
+	default int chatboxHeight() {
+		return 142;
+	}
+
+	@Range(min = 300, max = 519)
+	@ConfigItem(keyName = "chatboxWidth", name = "Chatbox Width", description = "Set the width of the chatbox")
+	default int chatboxWidth() {
+		return 519;
 	}
 }
--- src/main/java/com/resizablechatbox/ResizableChatboxPlugin.java
+++ src/main/java/com/resizablechatbox/ResizableChatboxPlugin.java
@@ -5,17 +5,23 @@ import javax.inject.Inject;
 import com.google.inject.Provides;
 
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.WidgetLoaded;
+import net.runelite.api.gameval.InterfaceID;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetSizeMode;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
-@PluginDescriptor(name = "Resizable Chatbox")
+@PluginDescriptor(name = "Resizable Chatbox", description = "Allows to change chatbox dimensions when playing with resizable layout.", tags = {
+		"layout", "ui", "chat", "chatbox", "resizable" })
 public class ResizableChatboxPlugin extends Plugin {
 	@Inject
 	private Client client;
@@ -23,21 +29,89 @@ public class ResizableChatboxPlugin extends Plugin {
 	@Inject
 	private ResizableChatboxConfig config;
 
+	@Inject
+	private ClientThread clientThread;
+
 	@Override
 	protected void startUp() throws Exception {
-		log.info("Example started!");
+		log.info("Resizable Chatbox started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception {
-		log.info("Example stopped!");
+		log.info("Resizable Chatbox stopped!");
 	}
 
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged) {
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN) {
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+		log.info("Game state changed to: {}", gameStateChanged.getGameState());
+	}
+
+	@Subscribe
+	public void onWidgetLoaded(WidgetLoaded widgetLoaded) {
+		if (widgetLoaded.getGroupId() == InterfaceID.CHATBOX) {
+			log.info("Chatbox widget loaded, setting size");
+			clientThread.invoke(this::setChatboxSizeFromConfig);
 		}
+
+		if (widgetLoaded.getGroupId() == InterfaceID.TOPLEVEL_OSRS_STRETCH ||
+				widgetLoaded.getGroupId() == InterfaceID.TOPLEVEL_PRE_EOC) {
+			log.info("Resizable viewport loaded, setting chatbox size");
+			clientThread.invoke(this::setChatboxSizeFromConfig);
+		}
+	}
+
+	@Subscribe
+	public void onConfigChanged(ConfigChanged configChanged) {
+		if (!configChanged.getGroup().equals("resizablechatbox")) {
+			return;
+		}
+
+		if (configChanged.getKey().equals("chatboxHeight") || configChanged.getKey().equals("chatboxWidth")) {
+			log.info("Config changed: {} = {}", configChanged.getKey(), configChanged.getNewValue());
+
+			clientThread.invoke(this::setChatboxSizeFromConfig);
+		}
+	}
+
+	Widget getViewportChatboxParent() {
+		Widget resizableModernChatboxParent = client
+				.getWidget(InterfaceID.ToplevelPreEoc.CHAT_CONTAINER);
+		Widget resizableClassicChatboxParent = client.getWidget(InterfaceID.ToplevelOsrsStretch.CHAT_CONTAINER);
+
+		if (resizableModernChatboxParent != null && !resizableModernChatboxParent.isHidden()) {
+			return resizableModernChatboxParent;
+		}
+
+		if (resizableClassicChatboxParent != null && !resizableClassicChatboxParent.isHidden()) {
+			return resizableClassicChatboxParent;
+		}
+
+		return null;
+	}
+
+	private void setChatboxSizeFromConfig() {
+		if (client.getGameState() != GameState.LOGGED_IN || !client.isResized()) {
+			return;
+		}
+
+		Widget chatboxParent = getViewportChatboxParent();
+
+		if (chatboxParent == null) {
+			log.info("Chatbox widget not available yet");
+			return;
+		}
+
+		log.info("Setting chatbox size: width={}, height={}", config.chatboxWidth(), config.chatboxHeight());
+
+		int heightPadding = 32;
+		int widthPadding = 4;
+
+		chatboxParent.setOriginalHeight(config.chatboxHeight() + heightPadding);
+		chatboxParent.setOriginalWidth(config.chatboxWidth() + widthPadding);
+		chatboxParent.setHeightMode(WidgetSizeMode.ABSOLUTE);
+		chatboxParent.setWidthMode(WidgetSizeMode.ABSOLUTE);
+		chatboxParent.revalidate();
 	}
 
 	@Provides
